cf857c593f4202560a52d8ecfe2798b3d3ea5261
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-10-01 17:57:35 -0300

    Primera vista

---
+++ resources/views/nuevo-vacunado-form.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.app')
+@section('contenido')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <!--Formulario para el registro de un nuevo vacunado-->
+        <h1>Formulario de nuevo vacunado</h1>
+    </div>
+    <form action="route('ingresarruta')" method=POST enctype="multipart/form-data">
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="nombre">Nombre</label>
+            </div>
+            <div class="col-8">
+                <input type="text" class="form-control" value="{{old('nombre')}}" id="nombre" name="nombre" required>
+                @error('Nombre')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="apellido">Apellido</label>
+            </div>
+            <div class="col-8">
+                <input type="text" class="form-control" value="{{old('apellido')}}" id="apellido" name="apellido" required>
+                @error('Apellido')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="fecha_nac">Fecha de nac</label>
+            </div>
+            <div class="col-8">
+                <input type="date" class="form-control" value="NULL" id="fecha_nac" placeholder="dd/mm/aaaa" name="fecha_nac" required>
+                @error('Fecha de nac')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="dni">DNI</label>
+            </div>
+            <div class="col-8">
+                <input type="number" class="form-control" value="{{old('dni')}}" id="dni" name="dni" required>
+                @error('DNI')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="dni">Comorbilidad</label>
+            </div>
+            <div class="col-8" style="display:inline-flex">
+                <div class="form-check ">
+                    <input 
+                    onclick="document.getElementById('descripcion').disabled = false;"
+                    class="form-check-input" type="radio" name="comorbilidad" id="Si" value="Si">
+                    <label class="form-check-label pr-3" for="si">Si</label>
+                </div>
+                <div class="form-check">
+                    <input 
+                    onclick="document.getElementById('descripcion').disabled = true;"
+                    class="form-check-input" type="radio" name="comorbilidad" id="No" value="No" checked>
+                    <label class="form-check-label" for="No">No</label>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="descripcion">Descripción</label>
+            </div>
+            <div class="col-8">
+                <input type="text" class="form-control" value="{{old('descripcion')}}" placeholder="" id="descripcion" name="descripcion" disabled="disabled">
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="nombre">Sexo</label>
+            </div>
+            <div class="col-8">
+                <select class="form-select">
+                    <option selected>Elija una opcion</option>
+                    <option value="F">Femenino</option>
+                    <option value="M">Masculino</option>
+                </select>
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="fecha_vac">Fecha de vacunacion</label>
+            </div>
+            <div class="col-8">
+                <input type="date" class="form-control" value="NULL" id="fecha_vac" placeholder="dd/mm/aaaa" name="fecha_vac" required>
+                @error('Fecha de vacunacion')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            <div class="col-2">
+                <label class="form-label fw-bold" for="codigo">Código de vacuna</label>
+            </div>
+            <div class="col-8">
+                <input type="text" class="form-control" value="{{old('codigo')}}" id="codigo" name="codigo" required>
+                @error('Nombre')<small>*{{$message}}</small>@enderror
+            </div>
+        </div>
+        <div class="row mb-3 justify-content-center">
+            @csrf 
+            @method('POST')
+            <div class="m-3">
+                <button type="submit" class="btn btn-success">Guardar</button>
+            </div>
+            <div class="m-3">
+                <button type="submit" class="btn btn-success">Cancelar</button>
+            </div>
+        </div>
+    </form>
+   
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/form', function () {
+    return view('nuevo-vacunado-form');
+});
