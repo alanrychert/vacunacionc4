1262034177bb034300e5f44ae335f3949d1a2fcb
matias rios <rios.matias998@gmail.com>
2021-10-03 13:54:05 -0300

    Revert "correccion de base de datos"
    This reverts commit fa53deff83f97d2d3d83f3cb060cf685d5d12c37.

--- database/migrations/2013_10_02_012858_create_sanitary_regions_table.php
+++ database/migrations/2013_10_02_012858_create_sanitary_regions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSanitaryRegionscosararaTable extends Migration
+class CreateSanitaryRegionTable extends Migration
 {
     /**
      * Run the migrations.
