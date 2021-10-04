ff1592ce7363f6f0d98e48a466d3d6789022a7d7
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-10-04 15:34:27 -0300

    Cambio de nombre

--- app/Models/Vaccine.php
+++ app/Models/Vaccine.php
@@ -24,7 +24,7 @@ class Vaccine extends Model
      * 
      * @var array
      */
-    protected $fillable = ['suffix_code','dose'];
+    protected $fillable = ['vaccine_number'];
 
     protected $guarded = ['id'];
 
