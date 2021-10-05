cb98ccfda71cd234cc246412fad8c159e23c3f9b
matias rios <rios.matias998@gmail.com>
2021-10-04 21:44:35 -0300

    correccion seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
+        $this->call(RoleSeeder::class);
         $this->call(ProvinceSeeder::class);
         $this->call(SanitaryRecionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(TypeOfVaccineSeeder::class);
-        $this->call(RoleSeeder::class);
+        
     }
 }
