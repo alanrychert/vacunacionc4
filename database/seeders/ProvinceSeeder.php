80063fc3f17b00832e531e2d29f19f121c8162ca
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2021-10-05 23:36:12 -0300

    Provincias

--- database/seeders/ProvinceSeeder.php
+++ database/seeders/ProvinceSeeder.php
@@ -14,8 +14,12 @@ class ProvinceSeeder extends Seeder
      */
     public function run()
     {
-        Province::create([
-            'name' => 'Buenos Aires',
-        ]);
+        $provincias = array('Buenos Aires', 'C.A.B.A.', 'Catamarca', 'Chaco', 'Chubut', 'Córdoba', 'Corrientes', 'Entre Ríos', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquén', 'Río Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucumán');
+
+        foreach ($provincias as $nombre) {
+            Province::create([
+                'name' => $nombre,
+            ]);
+        }
     }
 }
