d515a8dd84dec732c2ce4eb5871df397f285b65b
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2021-10-09 21:14:22 -0300

    Cambio navbar

--- resources/views/topbar-header.blade.php
+++ resources/views/topbar-header.blade.php
@@ -9,45 +9,48 @@
       <h1>Registro de Vacunacion Argentino</h1>
     </div>
     
-    <nav id="navbar" class="navbar justify-content-start">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <ul>
-        <li><a href="{{ route('index') }}">Inicio</a></li>
-       
-        @can('admin.create')
-        <li><a href="{{ route('vaccinated.index') }}">Vacunados</a></li>
-        @endcan
-        @can('operator.create')
-        <li><a href="{{ route('vaccinated.index') }}">Vacunados</a></li>
-        @endcan
-        @can('vaccinated.load')
-        <li><a href="{{ route('vaccinated.create') }}">Nuevo vacunado</a></li>
-        @endcan
-        @can('operator.create')
-        <li><a href="{{ route('batch.create') }}">Nuevo lote</a></li>  
-        @endcan
-        @can('admin.create')
-        <li><a href="{{ route('register') }}">Nuevo usuario</a></li>
-        @endcan
-        @can('operator.create')
-        <li><a href="{{ route('register') }}">Nuevo usuario</a></li>
-        @endcan
-        @auth
-        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            
+          <li class="nav-item"><a class="nav-link" href="{{ route('index') }}">Inicio</a></li>
+          @can('admin.create')
+            <li class="nav-item"><a class="nav-link" href="{{ route('vaccinated.index') }}">Vacunados</a></li>
+          @endcan
+          @can('operator.create')
+            <li class="nav-item"><a class="nav-link" href="{{ route('vaccinated.index') }}">Vacunados</a></li>
+          @endcan
+          @can('vaccinated.load')
+            <li class="nav-item"><a class="nav-link" href="{{ route('vaccinated.create') }}">Nuevo vacunado</a></li>
+          @endcan
+          @can('operator.create')
+            <li class="nav-item"><a class="nav-link" href="{{ route('batch.create') }}">Nuevo lote</a></li>  
+          @endcan
+          @can('admin.create')
+            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Nuevo usuario</a></li>
+          @endcan
+          @can('operator.create')
+            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Nuevo usuario</a></li>
+          @endcan
+          @auth
+            <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a></li>
         
-        <form method="POST" action="{{ route('logout') }}">
+          <form method="POST" action="{{ route('logout') }}">
           @csrf
-          <li><a href="cerrarSesion" onclick="event.preventDefault();this.closest('form').submit();">
-            Cerrar Sesión
-          </a></li>
-        </form>
+            <li class="nav-item"><a class="nav-link" href="cerrarSesion" onclick="event.preventDefault();this.closest('form').submit();">
+              Cerrar Sesión
+            </a></li>
+          </form>
 
         @else
-        <li><a href="{{ route('login') }}">
-          Ingresar
-        </a></li>
+          <li class="nav-item"><a href="{{ route('login') }}">
+            Ingresar
+          </a></li>
 
-        @endauth
-      </ul>
+            @endauth
+          </ul>
+        </div>
       <i class="bi mobile-nav-toggle d-none bi-x"></i>
     </nav><!-- .navbar -->
 </header><!-- End Header -->
