af4695a9cf5320572f897b72469d92fc7a9f5a7d
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-10-19 14:29:53 -0300

    esta roto we

--- resources/views/vaccinateds.blade.php
+++ resources/views/vaccinateds.blade.php
@@ -2,21 +2,60 @@
 @section('contenido')
 <div class="container-fluid col-8">
     <div class="row">
-            <div class="col">
-                <input type="text" id="myInput" onkeyup="myFunction(1)" class="form-control" placeholder="Filtrar por nombre">
-            </div>
-            <div class="col-8">
-                <select onchange="get(this.value)" class="form-select" name="selectedFilter">
-                    <option selected value="1">1 dosis por provincia</option>
-                    <option value="2">1 dosis por región sanitaria</option>
-                    <option value="3">2 dosis por provincia</option>
-                    <option value="4">Comorbilidad</option>
-                    <option value="5">Edad</option>
-                    <option value="6">Fecha de vacunación</option>
-                </select>
-            </div>
-        <br>
-        <br>
+        <div class="col-8">
+            <select onchange="get(this.value)" class="form-select" name="selectedFilter">
+                <option selected value="1">1 dosis por provincia</option>
+                <option value="2">1 dosis por región sanitaria</option>
+                <option value="3">2 dosis por provincia</option>
+                <option value="4">Comorbilidad</option>
+                <option value="5">Edad</option>
+                <option value="6">Fecha de vacunación</option>
+            </select>
+        </div>
+        <div class="col-8">
+            <select onchange="myFunction(9,this.value)" id ="province" name="province" class="form-select block appearance-none mt-1 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value="Buenos Aires">Buenos Aires</option>
+                <option value="C.A.B.A">C.A.B.A</option>
+                <option value="Catamarca">Catamarca</option>
+                <option value="Chaco">Chaco</option>
+                <option value="Chubut">Chubut</option>
+                <option value="Córdoba">Córdoba</option>
+                <option value="Corrientes">Corrientes</option>
+                <option value="Entre Ríos">Entre Ríos</option>
+                <option value="Formosa">Formosa</option>
+                <option value="Jujuy">Jujuy</option>
+                <option value="La Pampa">La Pampa</option>
+                <option value="La Rioja">La Rioja</option>
+                <option value="Mendoza">Mendoza</option>
+                <option value="Misiones">Misiones</option>
+                <option value="Neuquén">Neuquén</option>
+                <option value="Río Negro">Río Negro</option>
+                <option value="Salta">Salta</option>
+                <option value="San Juan">San Juan</option>
+                <option value="San Luis">San Luis</option>
+                <option value="Santa Cruz">Santa Cruz</option>
+                <option value="Santa Fe">Santa Fe</option>
+                <option value="Santiago del Estero">Santiago del Estero</option>
+                <option value="Tierra del Fuego">Tierra del Fuego</option>
+                <option value="Tucumán">Tucumán</option>
+            </select>   
+        </div>
+        <div class="col-8">
+            <select onchange="myFunction(3,this.value)" id="sanitary_region" name="sanitary_region" class="form-select block appearance-none mt-1 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value="Region Sanitaria I">Region Sanitaria I</option>
+                <option value="Region Sanitaria II">Region Sanitaria II</option>
+                <option value="Region Sanitaria III">Region Sanitaria III</option>
+                <option value="Region Sanitaria IV">Region Sanitaria IV</option>
+                <option value="Region Sanitaria V">Region Sanitaria V</option>
+                <option value="Region Sanitaria VI">Region Sanitaria VI</option>
+                <option value="Region Sanitaria VII">Region Sanitaria VII</option>
+                <option value="Region Sanitaria VIII">Region Sanitaria VIII</option>
+                <option value="Region Sanitaria IX">Region Sanitaria IX</option>
+                <option value="Region Sanitaria X">Region Sanitaria X</option>
+                <option value="Region Sanitaria XI">Region Sanitaria XI</option>
+                <option value="Region Sanitaria XII">Region Sanitaria XII</option>
+            </select>   
+        </div>
     </div>
     <div class="table-responsive">
         <table id="myTable" class="table content-table">
@@ -49,50 +88,22 @@
         console.log(value);
         switch (value) {
             case '1': //filtra por primera dosis y provincia
-                const myTable = $("#myTable");
                 console.log("entré");
+                function hide(){
+                    document.getElementById('sanitary_region').style.visibility = 'hidden';
+                    $('#sanitary_region').style.visibility = 'hidden';
+                }
                 $.ajax({
                 url:"{{ route('vaccinated.byDose') }}",
                 method: "GET",
-                data:{ dose:FIRSTDOSE},
+                data: {dose:FIRSTDOSE},
                 success: 
                     function(data){
-                        data.forEach(result => {
-                            const myRow = $("<tr>");
-                            const dniTD = $("<td>");
-                            dniTD.text(result.dni);
-                            const sexTD = $("<td>");
-                            sexTD.text(result.sex);
-                            const nameTD = $("<td>");
-                            nameTD.text(result.name);
-                            const last_nameTD = $("<td>");
-                            last_nameTD.text(result.last_name);
-                            const comorbidityTD = $("<td>");
-                            comorbidityTD.text(result.comorbidity);
-                            const doseTD = $("<td>");
-                            doseTD.text(result.dose);
-                            const date_of_birthTD = $("<td>");
-                            date_of_birthTD.text(result.date_of_birth);
-                            const date_of_vaccinationTD = $("<td>");
-                            date_of_vaccinationTD.text(result.date_of_vaccination);
-                            const regionTD = $("<td>");
-                            regionTD.text(result.region);
-                            const provinceTD = $("<td>");
-                            provinceTD.text(result.province);
-                            myRow.append(dniTD);
-                            myRow.append(sexTD);
-                            myRow.append(nameTD);
-                            myRow.append(last_nameTD);
-                            myRow.append(comorbidityTD);
-                            myRow.append(doseTD);
-                            myRow.append(date_of_birthTD);
-                            myRow.append(date_of_vaccinationTD);
-                            myRow.append(regionTD);
-                            myRow.append(provinceTD);
-                            myTable.append(myRow);
-                        })
+                        parseResultToTable(data);
+                        hide()
                     }
-        })
+                })
+                
                 break;
             case 2:
                 
@@ -114,26 +125,95 @@
         }
     }
 
-      function myFunction(j) {
-          var input, filter, table, tr, td, i, txtValue;
-          input = document.getElementById("myInput");
-          filter = input.value.toUpperCase();
-          table = document.getElementById("myTable");
-          tbody = table.getElementsByTagName("tbody");
-          tr = tbody[0].getElementsByTagName("tr");
-          for (i = 0; i < tr.length; i++) {
-              td = tr[i].getElementsByTagName("td")[j];
-              if (!td) {
-                return 
-              }
-              txtValue = td.textContent || td.innerText;
-              if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                  tr[i].style.display = "";
-              } else {
-                  tr[i].style.display = "none";
-              }
-          }
-      }
+    function myFunction(j,value) {
+        var input, filter, table, tr, td, i, txtValue;
+        filter = value.toUpperCase();
+        table = document.getElementById("myTable");
+        tbody = table.getElementsByTagName("tbody");
+        tr = tbody[0].getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[j];
+            if (!td) {
+            return 
+            }
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase() == filter) {
+            tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 
     //Hacer función que haga append de todo el resultado
+    function parseResultToTable(results){
+        const myTable = $("#myTable");
+        myTable.empty();
+        const firstRow = $("<tr>");
+        const dniTH = $("<th>");
+            dniTH.text('DNI');
+            const sexTH = $("<th>");
+            sexTH.text('Sexo');
+            const nameTH = $("<th>");
+            nameTH.text('Nombre');
+            const last_nameTH = $("<th>");
+            last_nameTH.text('Apellido');
+            const comorbidityTH = $("<th>");
+            comorbidityTH.text('Comorbilidad');
+            const doseTH = $("<th>");
+            doseTH.text('Dosis');
+            const date_of_birthTH = $("<th>");
+            date_of_birthTH.text('Fecha de Nacimiento');
+            const date_of_vaccinationTH = $("<th>");
+            date_of_vaccinationTH.text('Fecha de vacunacion');
+            const regionTH = $("<th>");
+            regionTH.text('Region Sanitaria');
+            const provinceTH = $("<th>");
+            provinceTH.text('Provincia');
+            firstRow.append(dniTH);
+            firstRow.append(sexTH);
+            firstRow.append(nameTH);
+            firstRow.append(last_nameTH);
+            firstRow.append(comorbidityTH);
+            firstRow.append(doseTH);
+            firstRow.append(date_of_birthTH);
+            firstRow.append(date_of_vaccinationTH);
+            firstRow.append(regionTH);
+            firstRow.append(provinceTH);
+            myTable.append(firstRow);
+        results.forEach(result => {
+            const myRow = $("<tr>");
+            const dniTD = $("<td>");
+            dniTD.text(result.dni);
+            const sexTD = $("<td>");
+            sexTD.text(result.sex);
+            const nameTD = $("<td>");
+            nameTD.text(result.name);
+            const last_nameTD = $("<td>");
+            last_nameTD.text(result.last_name);
+            const comorbidityTD = $("<td>");
+            comorbidityTD.text(result.comorbidity);
+            const doseTD = $("<td>");
+            doseTD.text(result.dose);
+            const date_of_birthTD = $("<td>");
+            date_of_birthTD.text(result.date_of_birth);
+            const date_of_vaccinationTD = $("<td>");
+            date_of_vaccinationTD.text(result.date_of_vaccination);
+            const regionTD = $("<td>");
+            regionTD.text(result.region);
+            const provinceTD = $("<td>");
+            provinceTD.text(result.province);
+            myRow.append(dniTD);
+            myRow.append(sexTD);
+            myRow.append(nameTD);
+            myRow.append(last_nameTD);
+            myRow.append(comorbidityTD);
+            myRow.append(doseTD);
+            myRow.append(date_of_birthTD);
+            myRow.append(date_of_vaccinationTD);
+            myRow.append(regionTD);
+            myRow.append(provinceTD);
+            myTable.append(myRow);
+        })
+    }
   </script>
